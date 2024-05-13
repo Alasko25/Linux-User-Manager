59220fe40bb1eef3e96a2e9d66c125360600450b
darius-konsebo <149010056+darius-konsebo@users.noreply.github.com>
2024-05-13 22:19:48 +0100

    Update newUser.sh

--- scripts shell/newUser.sh
+++ scripts shell/newUser.sh
@@ -3,11 +3,21 @@
 # Fonction pour créer un nouvel utilisateur
 
     read -p "Nom d'utilisateur : " username
-    read -p "Mot de passe : " -s password
+    read -p "Mot de passe (Laisser vide pour aucun mot de passe) : " -s password
     echo
-    read -p "Groupe primaire : " primary_group
-    read -p "Groupe secondaire : " secondary_group
-    read -p "Répertoire personnel : " home_dir
+    read -p "Groupe primaire (Laisser vide pour par défaut) : " primary_group
+    read -p "Groupe secondaire (Laisser vide pour par défaut) : " secondary_group
+    read -p "Répertoire personnel (Laisser vide pour par défaut) : " home_dir
 
-    sudo useradd -m -g $primary_group -G $secondary_group -d $home_dir $username
-    echo "$username:$password" | sudo chpasswd
+    # Vérifier si les chaînes de caractères sont vides et les remplacer par les valeurs par défaut
+    [ -z "$primary_group" ] && primary_group=$username
+    [ -z "$secondary_group" ] && secondary_group=""
+    [ -z "$home_dir" ] && home_dir="/home/$username"
+
+    # Créer l'utilisateur en utilisant sudo useradd avec les paramètres fournis
+    sudo useradd -m -g "$primary_group" -G "$secondary_group" -d "$home_dir" "$username"
+
+    # Vérifier si un mot de passe a été fourni et le définir si nécessaire
+    if [ -n "$password" ]; then
+        echo "$username:$password" | sudo chpasswd
+    fi
