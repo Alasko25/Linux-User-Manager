66e8f0cd195f038e85ad7f240730609195440008
TOURE ALASSANE <149190657+Alasko25@users.noreply.github.com>
2024-05-13 23:56:17 +0100

    Update forkLuncher.c

--- C codes/forkLuncher.c
+++ C codes/forkLuncher.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-void exec_fork() {
-    printf("Exécution par création de sous-processus avec fork\n");
-    // Exemple de code : exécuter une commande en arrière-plan
-    printf("Lancement de la commande en arrière-plan...\n");
+void exec_fork(char *script_path) {
+    printf("Exécution du script shell en arrière-plan...\n");
+
     pid_t pid = fork(); // Création d'un nouveau processus
     
     if (pid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     } else if (pid == 0) { // Processus fils
-        sleep(2); // Exemple : exécute la commande 'sleep 2' en arrière-plan
-        printf("Commande lancée en arrière-plan.\n");
-        exit(EXIT_SUCCESS);
+        // Exécution du script shell
+        execlp("sh", "sh", script_path, NULL);
+        // Si execvp retourne, cela signifie qu'il y a eu une erreur
+        perror("execlp");
+        exit(EXIT_FAILURE);
     } else { // Processus parent
-        // Attendre que le processus fils se termine (ici, sleep 2)
+        // Attendre que le processus fils se termine
         wait(NULL);
+        printf("Le script shell a été exécuté en arrière-plan.\n");
     }
 }
 
-int main() {
-    exec_fork();
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <script_shell>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    exec_fork(argv[1]);
     return 0;
 }
