4d3fc786dfc54a09b620841a58375493bc0bc068
TOURE ALASSANE <149190657+Alasko25@users.noreply.github.com>
2024-05-13 22:15:32 +0100

    Update resetParams.sh

--- scripts shell/resetParams.sh
+++ scripts shell/resetParams.sh
@@ -5,5 +5,5 @@
     echo "Réinitialisation des paramètres par défaut..."
 
     # Remettre le chemin de sauvegarde par défaut
-    current_log_directory="$default_log_directory"
+    $current_log_directory="$default_log_directory"
 
