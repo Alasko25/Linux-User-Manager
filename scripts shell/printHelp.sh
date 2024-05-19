35f2c92e8f5a927e49842b15a52211fc0fb63a50
darius-konsebo <149010056+darius-konsebo@users.noreply.github.com>
2024-05-19 17:08:54 +0100

    Update printHelp.sh

--- scripts shell/printHelp.sh
+++ scripts shell/printHelp.sh
@@ -8,7 +8,7 @@
     echo "  -m : Modifier les informations d'un utilisateur"
     echo "  -d : Supprimer un utilisateur"
     echo "  -config : Configurer les droits sudoers"
-    echo "  -i : Surveiller l'intégrité des fichiers système"
+    echo "  -i : Redemare la surveillance des droits d'utilisation commandes lancés"
     echo "  -g : Générer des rapports"
     echo "  -h : Afficher cette aide"
     echo "  -f : Permet une exécution par création de sous-processus avec fork"
@@ -16,4 +16,3 @@
     echo "  -s : Exécute le programme dans un sous-shell"
     echo "  -l : Permet de spécifier un répertoire pour le stockage du fichier de journalisation"
     echo "  -r : Réinitialise les paramètres par défaut, utilisable uniquement par des administrateurs"
-
