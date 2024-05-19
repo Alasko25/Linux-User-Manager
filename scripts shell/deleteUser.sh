c58368888e97fdbf3689556b3c8b23c064a2a0a6
darius-konsebo <149010056+darius-konsebo@users.noreply.github.com>
2024-05-19 18:07:02 +0100

    Update deleteUser.sh

--- scripts shell/deleteUser.sh
+++ scripts shell/deleteUser.sh
@@ -1,8 +1,15 @@
-
 #!/bin/bash
 
 # Fonction pour supprimer un utilisateur
 
-    read -p "Nom d'utilisateur à supprimer : " username
+read -p "Nom d'utilisateur à supprimer : " username
 
-    sudo userdel -r $username > /dev/null
+# Vérifier si l'utilisateur existe
+if id "$username" &>/dev/null; then
+    # Supprimer l'utilisateur s'il existe
+    sudo userdel -r "$username" > /dev/null 2>&1
+    echo "Utilisateur $username supprimé avec succès."
+else
+    echo "Utilisateur $username non trouvé."
+    exit 1
+fi
