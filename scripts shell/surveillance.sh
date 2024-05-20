b82d724f9c3939fcb53482cdf271cc4451131097
darius-konsebo <149010056+darius-konsebo@users.noreply.github.com>
2024-05-20 04:33:26 +0100

    Update surveillance.sh

--- scripts shell/surveillance.sh
+++ scripts shell/surveillance.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-# Fonction pour surveiller les commandes et gérer les autorisations
+# Définition du nom du programme (remplacez 'yourprogramname' par le nom de votre programme)
+program_name="um"
+
+# Définition du répertoire de journalisation par défaut initial
+initial_default_log_directory="/var/log/$program_name/"
+
+# Nom du fichier pour sauvegarder le répertoire par défaut courant
+default_directory_file="/tmp/${program_name}_current_default_log_directory"
+
+# Initialiser le répertoire de journalisation courant au répertoire par défaut initial
+if [ -f "$default_directory_file" ]; then
+    current_default_log_directory=$(cat "$default_directory_file")
+else
+    current_default_log_directory="$initial_default_log_directory"
+    echo "$current_default_log_directory" > "$default_directory_file"
+fi
+
+current_log_directory="$current_default_log_directory"
 
 # Vérifier si le fichier de journalisation des commandes existe
 if [ ! -f "${current_log_directory}command.log" ]; then
-    touch ${current_log_directory}command.log
+    touch "${current_log_directory}command.log"
 fi
 
 # Définir une fonction pour enregistrer les commandes
 log_commande() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $USER: $1" >> ${current_log_directory}command.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $USER: $1" >> "${current_log_directory}command.log"
 }
 
 # Fonction pour vérifier le type de commande et les autorisations d'accès
@@ -36,15 +53,16 @@ verifier_commande() {
 
 # Fonction pour bloquer l'utilisateur en cas de demandes refusées répétées
 bloquer_utilisateur() {
-    today_denied_requests=$(grep -c "^$(date '+%Y-%m-%d') $USER.*refusée" ${current_log_directory}command.log)
+    today_denied_requests=$(grep -c "^$(date '+%Y-%m-%d') $USER.*refusée" "${current_log_directory}command.log")
     if [ "$today_denied_requests" -ge 5 ]; then
         echo "Blocage de l'activité pour $USER en raison de trop de demandes refusées."
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - $USER: Activité bloquée" >> ${current_log_directory}command.log
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - $USER: Activité bloquée" >> "${current_log_directory}command.log"
         exit 1
     fi
 }
 
-# Intercepter chaque commande exécutée
+
+# Configurer un piège pour intercepter les commandes avant leur exécution
 trap 'verifier_commande "$BASH_COMMAND"' DEBUG
 
 # Attendre en arrière-plan pour continuer la surveillance
