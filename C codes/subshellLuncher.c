ee813a5625457d897c816d2201d0f35739004321
darius-konsebo <149010056+darius-konsebo@users.noreply.github.com>
2024-05-20 08:29:34 +0100

    Update subshellLuncher.c

--- C codes/subshellLuncher.c
+++ C codes/subshellLuncher.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-void exec_subshell() {
+void exec_subshell(char* script[], int length) {
+    
     printf("Exécution dans un sous-shell\n");
-    // Exemple de code : exécuter une série de commandes dans un sous-shell
-    system("(echo \"Début du sous-shell...\"; ls -l; echo \"Fin du sous-shell\") > subshell_output.txt");
+    
+    int total_length = 0;
+    for (int i = 1; i < length; ++i) {
+        total_length += ( strlen(script[i]) + 1 );
+    }
+
+    // Allouer de la mémoire pour le tampon
+    char *buffer = (char *)malloc(total_length + 6); 
+    if (buffer == NULL) {
+        fprintf(stderr, "Erreur d'allocation de mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Concaténer tous les arguments dans le tampon
+    buffer[0] = '(';
+    buffer[1] = ' ';
+    buffer[2] = '\0'; 
+    for (int i = 1; i < length; ++i) {
+        strcat(buffer, script[i]);
+        strcat(buffer, " ");
+    }
+    
+    strcat(buffer, ")");
+    
+    system(buffer); 
+    
     printf("Exécution dans un sous-shell terminée.\n");
 }
 
-int main() {
-    exec_subshell();
+int main(int argc, char* argv[]) {
+    exec_subshell(argv, argc);
     return 0;
 }
